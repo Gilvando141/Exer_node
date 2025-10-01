54a0611fbd44df8f1b26eeabaf90127bf84164ed
Gilvando Alves <106853597+Gilvando141@users.noreply.github.com>
2025-09-30 22:56:54 -0300

    Update and rename HomeModel.js to clienteModel.js

---
+++ src/models/clienteModel.js
@@ -0,0 +1,27 @@
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const Cliente = sequelize.define('Cliente', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  telefone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  tableName: 'clientes', 
+});
+
+module.exports = Cliente;
